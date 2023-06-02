5aaa1e3fc6bde70fa0534e9fb14dd147c219fc34
Tom Dennis <tom.w.dennis@hotmail.co.uk>
2023-06-02 11:20:39 +0100

    root route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   resources :lists do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
+  root to: 'lists#index'
 end
